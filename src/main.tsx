c31546bcc4c3e8e56a28137c57ec2871f597bccc
hyunju1211 <hyunju0411@konkuk.ac.kr>
2025-11-11 13:51:31 +0900

    fix: 협업 규칙에 맞게 디렉토리 경로 수정,파일 삭제, 라우팅에 경로 추가

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './commute/shared/styles/global.css';
+import './shared/styles/global.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/shared/router/Router.tsx
+++ src/shared/router/Router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
+import Test from '../../pages/Test';
 import AuthPage from '../../commute/pages/AuthPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
+    element: <Test />,
+  },
+  {
+    path: '/auth',
     element: <AuthPage />,
   },
 ]);
